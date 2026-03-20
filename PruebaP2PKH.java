d7cc93c30be17c36ca53302f20dfba45db29a0ea
its-tony <tonycontacto361@gmail.com>
2026-03-20 11:31:43 -0600

    agregue la conexion p2p con el interprete

--- PruebaP2PKH.java
+++ PruebaP2PKH.java
@@ -1,39 +1,42 @@
 public class PruebaP2PKH {
 
     public static void main(String[] args) {
+        boolean trace = args.length > 0 && args[0].equalsIgnoreCase("--trace");
+        IOpcodeInterpreter interpreter = new Logica(trace);
 
-        IOpcodeInterpreter interpreter = new Logica(true);
+        System.out.println("-- Prueba de P2PKH con el interprete --\n");
 
-        System.out.println("-- Prueba de P2PKH --\n");
-
-        // Caso Válido
         String scriptValido =
-                "SIG_VALIDA PUBKEY1 " +
+                "SIG_PUBKEY1 PUBKEY1 " +
                 "OP_DUP OP_HASH160 HASH_PUBKEY1 OP_EQUALVERIFY OP_CHECKSIG";
 
-        System.out.println("-- Caso Válido --");
-        boolean resultadoValido = interpreter.execute(scriptValido);
-        System.out.println("Resultado: " + resultadoValido);
-        System.out.println();
-
-        // Caso inválido (Hash Incorrecto)
         String scriptHashIncorrecto =
-                "SIG_VALIDA PUBKEY1 " +
+                "SIG_PUBKEY1 PUBKEY1 " +
                 "OP_DUP OP_HASH160 HASH_FAKE OP_EQUALVERIFY OP_CHECKSIG";
 
-        System.out.println("-- Caso Hash Incorrecto --");
-        boolean resultadoHash = interpreter.execute(scriptHashIncorrecto);
-        System.out.println("Resultado: " + resultadoHash);
-        System.out.println();
-
-        // Caso Inválido (Firma Incorrecta)
         String scriptFirmaIncorrecta =
-                "SIG_INVALIDA PUBKEY1 " +
+                "SIG_FAKE PUBKEY1 " +
                 "OP_DUP OP_HASH160 HASH_PUBKEY1 OP_EQUALVERIFY OP_CHECKSIG";
 
-        System.out.println("-- Caso Firma Incorrecta --");
-        boolean resultadoFirma = interpreter.execute(scriptFirmaIncorrecta);
-        System.out.println("Resultado: " + resultadoFirma);
+        String scriptConError = "OP_ELSE";
+
+        ejecutarCaso(interpreter, "Caso Valido", scriptValido);
+        ejecutarCaso(interpreter, "Caso Hash Incorrecto", scriptHashIncorrecto);
+        ejecutarCaso(interpreter, "Caso Firma Incorrecta", scriptFirmaIncorrecta);
+        ejecutarCaso(interpreter, "Caso Error de Script", scriptConError);
+    }
+
+    private static void ejecutarCaso(IOpcodeInterpreter interpreter, String titulo, String script) {
+        System.out.println("-- " + titulo + " --");
+        System.out.println("Script: " + script);
+
+        boolean resultado = interpreter.execute(script);
+        System.out.println("Resultado: " + resultado);
+
+        if (!resultado && interpreter.getLastError() != null) {
+            System.out.println("Error del interprete: " + interpreter.getLastError());
+        }
+
         System.out.println();
     }
 }
